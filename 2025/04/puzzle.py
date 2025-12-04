f714036fa2e66c567014e205bc8f402c03e8eedb
elliome <elliotluke.emmerson@gmail.com>
2025-12-04 22:10:14 +0000

    use tuple as dict key

--- 2025/04/puzzle.py
+++ 2025/04/puzzle.py
@@ -5,13 +5,13 @@ with open('input.txt') as f:
     lines: List[str] = f.read().splitlines()
 
 start_time:float = time.time()
-grid:Dict[str, int] = {}
+grid:Dict[tuple[int,int], int] = {}
 part_1_sum:int = 0
 part_2_sum:int = 0
 
 for y, line in enumerate(lines):
     for x, char in enumerate(line):
-        grid[f'{x},{y}'] = 1 if char == '@'else 0
+        grid[x,y] = 1 if char == '@' else 0
 
 
 for y, line in enumerate(lines):
@@ -23,8 +23,8 @@ for y, line in enumerate(lines):
 
         for relative_x in range(x + -1,  x + 2):
             for relative_y in range(y + -1,  y + 2):
-                if f'{relative_x},{relative_y}' in grid:
-                    neighbours.append(grid[f'{relative_x},{relative_y}'])
+                if (relative_x,relative_y) in grid:
+                    neighbours.append(grid[relative_x,relative_y])
 
         if sum(neighbours) - 1 < 4:
             part_1_sum += 1
@@ -35,21 +35,23 @@ start_time = time.time()
 has_removed_one:bool = True
 while has_removed_one:
     has_removed_one = False
-    for y, line in enumerate(lines):
-        for x, char in enumerate(line):
-            if grid[f'{x},{y}'] == 0:
-                continue
+    inp = list(grid.values())
+    for i, char in enumerate(inp):
+        y = i // len(lines[0])
+        x = i - y * len(lines[0])
 
-            neighbour_count:int = 0
+        if char == 0:
+            continue
 
-            for relative_x in range(x + -1,  x + 2):
-                for relative_y in range(y + -1,  y + 2):
-                    if f'{relative_x},{relative_y}' in grid:
-                        neighbour_count += grid[f'{relative_x},{relative_y}']
+        neighbour_count:int = 0
 
-            if neighbour_count - 1 < 4:
-                part_2_sum += 1
-                grid[f'{x},{y}'] = 0
-                has_removed_one = True
+        for relative_x in range(x + -1,  x + 2):
+            for relative_y in range(y + -1,  y + 2):
+                if (relative_x,relative_y) in grid:
+                    neighbour_count += grid[relative_x,relative_y]
+        if neighbour_count - 1 < 4:
+            part_2_sum += 1
+            grid[x,y] = 0
+            has_removed_one = True
 
 print(f"Part 2 {part_2_sum} in {time.time()-start_time} seconds")
