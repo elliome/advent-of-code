7bea93f1f6400cf9f2b0544834abd8aeaafa1f8b
Elliot Emmerson <elliote@fisheyehq.com>
2025-12-04 21:44:04 +0000

    2025 day 4

---
+++ 2025/04/puzzle.py
@@ -0,0 +1,55 @@
+import time
+from typing import List, Dict
+
+with open('input.txt') as f:
+    lines: List[str] = f.read().splitlines()
+
+start_time:float = time.time()
+grid:Dict[str, int] = {}
+part_1_sum:int = 0
+part_2_sum:int = 0
+
+for y, line in enumerate(lines):
+    for x, char in enumerate(line):
+        grid[f'{x},{y}'] = 1 if char == '@'else 0
+
+
+for y, line in enumerate(lines):
+    for x, char in enumerate(line):
+        if char == ".":
+            continue
+
+        neighbours: List[int] = []
+
+        for relative_x in range(x + -1,  x + 2):
+            for relative_y in range(y + -1,  y + 2):
+                if f'{relative_x},{relative_y}' in grid:
+                    neighbours.append(grid[f'{relative_x},{relative_y}'])
+
+        if sum(neighbours) - 1 < 4:
+            part_1_sum += 1
+
+print(f"Part 1 {part_1_sum} in {time.time()-start_time} seconds")
+start_time = time.time()
+
+has_removed_one:bool = True
+while has_removed_one:
+    has_removed_one = False
+    for y, line in enumerate(lines):
+        for x, char in enumerate(line):
+            if grid[f'{x},{y}'] == 0:
+                continue
+
+            neighbour_count:int = 0
+
+            for relative_x in range(x + -1,  x + 2):
+                for relative_y in range(y + -1,  y + 2):
+                    if f'{relative_x},{relative_y}' in grid:
+                        neighbour_count += grid[f'{relative_x},{relative_y}']
+
+            if neighbour_count - 1 < 4:
+                part_2_sum += 1
+                grid[f'{x},{y}'] = 0
+                has_removed_one = True
+
+print(f"Part 2 {part_2_sum} in {time.time()-start_time} seconds")
