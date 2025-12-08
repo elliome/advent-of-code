fe9365ad0f2ac7602b7f63d1482afe78e17d5a6b
Elliot Emmerson <elliote@fisheyehq.com>
2025-12-08 18:20:18 +0000

    2025 day 8 cleanup

--- 2025/08/puzzle.py
+++ 2025/08/puzzle.py
@@ -2,35 +2,20 @@ import functools
 import math
 import operator
 import time
-from linecache import cache
 from typing import List, Dict, Optional
-from xml.etree.ElementTree import tostring
 
 with open('input.txt') as f:
     junction_boxes: List[tuple[int,int,int]] = [tuple(int(n) for n in x.split(",")) for x in f.read().splitlines()]
 
-# print(junction_boxes)
+start_time = time.time()
 junction_box_to_network_lookup: Dict[tuple[int, int, int], Optional[int]] = {}
 network_to_junction_box: Dict[int, set[tuple[int, int, int]]] = {}
-
-network_counter: int = 0
-
-# add circuits to dict
-start_time = time.time()
-
-# print(f"time to build dict {time.time() - start_time} seconds")
-# print(junction_box_to_network_lookup)
+all_paths_between_junction_boxes: List[Path] = []
 
 @functools.cache
 def find_distance (a: tuple[int,int,int], b: tuple[int,int,int]) -> int:
     return math.sqrt(math.pow(abs(a[0] - b[0]), 2) + math.pow(abs(a[1] - b[1]), 2) + math.pow(abs(a[2] - b[2]), 2))
 
-# def is_junction_boxes_in_same_network(a: tuple[int,int,int], b: tuple[int,int,int]) -> bool:
-#     if a not in junction_box_to_network_lookup or b not in junction_box_to_network_lookup:
-#         return False
-#
-#     return junction_box_to_network_lookup[a] == junction_box_to_network_lookup[b]
-#
 def join_network(a,b):
     if a == b:
         return
@@ -40,55 +25,6 @@ def join_network(a,b):
     for junction_box in network_b_junction_boxes:
         junction_box_to_network_lookup[junction_box] = a
         network_to_junction_box[a].add(junction_box)
-#
-#
-# # print(find_distance((162,817,812),(425,690,689)))
-#
-# for i in range(1000):
-#     shortest_path:int = math.inf
-#     nodes_for_shortest_path:List[tuple[int,int,int]] = []
-#
-#     for index_a, junction_box_a in enumerate(junction_boxes):
-#         for junction_box_b in junction_boxes[index_a + 1:]:
-#             if is_junction_boxes_in_same_network(junction_box_a, junction_box_b):
-#                 continue
-#
-#             if find_distance(junction_box_a, junction_box_b) < shortest_path:
-#                 shortest_path = find_distance(junction_box_a, junction_box_b)
-#                 nodes_for_shortest_path = [junction_box_a, junction_box_b]
-#
-#     if not nodes_for_shortest_path:
-#         break
-#
-#     network = network_counter
-#     if nodes_for_shortest_path[0] in junction_box_to_network_lookup and nodes_for_shortest_path[1] in junction_box_to_network_lookup:
-#         join_network(junction_box_to_network_lookup[nodes_for_shortest_path[0]],junction_box_to_network_lookup[nodes_for_shortest_path[1]])
-#         break
-#
-#     elif nodes_for_shortest_path[0] in junction_box_to_network_lookup:
-#         network = junction_box_to_network_lookup[nodes_for_shortest_path[0]]
-#         junction_box_to_network_lookup[nodes_for_shortest_path[1]] = network
-#         network_to_junction_box[network].append(junction_box_to_network_lookup[nodes_for_shortest_path[0]])
-#     elif nodes_for_shortest_path[1] in junction_box_to_network_lookup:
-#         network = junction_box_to_network_lookup[nodes_for_shortest_path[1]]
-#         junction_box_to_network_lookup[nodes_for_shortest_path[0]] = network
-#         network_to_junction_box[network].append(junction_box_to_network_lookup[nodes_for_shortest_path[1]])
-#     else:
-#         if network in network_to_junction_box:
-#             network_to_junction_box[network].append(nodes_for_shortest_path[0])
-#             network_to_junction_box[network].append(nodes_for_shortest_path[1])
-#         else:
-#             network_to_junction_box[network] = [*nodes_for_shortest_path]
-#
-#         for t in nodes_for_shortest_path:
-#             junction_box_to_network_lookup[t] = network
-#
-#     network_counter += 1
-#
-# print(junction_box_to_network_lookup)
-# print(network_to_junction_box)
-# print(sorted([len(network_to_junction_box[l]) for l in network_to_junction_box], reverse=True)[:3])
-# print(functools.reduce(operator.mul, sorted([len(network_to_junction_box[l]) for l in network_to_junction_box], reverse=True)[:3]))
 
 
 class Path:
@@ -103,9 +39,6 @@ class Path:
         return f"{self.connected_junction_boxes[0]},{self.connected_junction_boxes[1]},{self.distance}"
 
 
-all_paths_between_junction_boxes: List[Path] = []
-
-# start_time = time.time()
 for index_a, junction_box_a in enumerate(junction_boxes):
     for junction_box_b in junction_boxes[index_a+1:]:
         all_paths_between_junction_boxes.append(Path((junction_box_a, junction_box_b)))
